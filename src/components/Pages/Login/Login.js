4fc4976d7dc3d041ccf6f01b840298b18b7a2a6e
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-11-30 21:50:53 +0600

    login functionality added

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -1,37 +1,76 @@
 import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import toast from 'react-hot-toast';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../Context/AuthProvider/AuthProvider';
  
-const Login = () => {
+const Register = () => {
    const { register, handleSubmit } = useForm();
-   
+   const { createUser, googleLogin, updateUser } = useContext(AuthContext)
+   const [userEmail, setUserEmail] = useState(null);
+   const navigate = useNavigate();
+   const onSubmit = data => {
+       createUser(data.email, data.password)
+           .then(user => {
+               const userInfo = {
+                   displayName: data.name
+               }
+               updateUser(userInfo)
+                   .then(() => {
+                       saveUserToDB(data.name, data.email)
+                   })
+                   .catch(err => console.error(err))
+           })
+           .catch(err => console.error(err))
+   };
+   const handleGoogleLogin = () => {
+       googleLogin()
+           .then(result => {
+               const user = result.user
+               saveUserToDB(user.displayName, user.email)
+           })
+           .catch(err => console.error(err))
+   }
+   const saveUserToDB = (name, email) => {
+       const user = { name, email };
+       fetch('http://localhost:4000/users', {
+           method: 'post',
+           headers: {
+               'content-type': 'application/json'
+           },
+           body: JSON.stringify(user)
+       })
+           .then(res => res.json())
+           .then(data => {
+               setUserEmail(user.email)
+           })
+   }
    return (
        <div className='h-[800px] flex justify-center items-center' >
            <div className='w-11/12 md:w-3/4 lg:w-4/12 shadow-xl p-10 rounded-xl'>
-               <form onSubmit={handleSubmit()}>
-                   <h1 className='text-2xl text-center mb-5'>Login</h1>
+               <form onSubmit={handleSubmit(onSubmit)}>
+                   <h1 className='text-2xl text-center mb-5'>Register</h1>
+                   <div className='form-contorl'>
+                       <label className='label label-text'>Name</label>
+                       <input type='text' className='input input-bordered w-full' {...register("name", { required: true, maxLength: 20 })} />
+                   </div>
                    <div className='form-contorl'>
                        <label className='label label-text'>Email</label>
-                       <input type='email' className='input input-bordered w-full' {...register("email", { required: true })} />
+                       <input type='emafil' className='input input-bordered w-full' {...register("email", { required: true })} />
                    </div>
                    <div className='form-control'>
                        <label className='label label-text'>Password</label>
                        <input type='password' {...register("password", { required: true })} className='input input-bordered w-full' />
-                       <label className='label label-text'>Forgot Password ?</label>
                    </div>
                    <div className='form-control mb-3'>
-                       <input type="submit" value='Login' className='btn btn-black mt-5' />
+                       <input type="submit" value='Register' className='btn btn-accent mt-5' />
                    </div>
-                   <p className='text-center'>New to Doctors Portal? <Link className='text-rose-500 underline' to='/register'>Create new Account</Link></p>
+                   <p className='text-center'>Already Have an Account? <Link className='text-rose-500 underline' to='/login'>Please Login</Link></p>
                    <div className="divider">OR</div>
-               </form>
-               <button className='btn w-full bg-white text-accent text-lg font-normal hover:bg-black hover:text-white'>Continue With Google</button>
-           </div>
-       </div>
+               </form >
+               <button className='btn w-full bg-white text-accent text-lg font-normal hover:bg-black hover:text-white' onClick={handleGoogleLogin}>Continue With Google</button>
+           </div >
+       </div >
    );
 };
  
-export default Login;
-
+export default Register;
