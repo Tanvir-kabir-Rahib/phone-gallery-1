8baea6666549c445674c9a5dbda82578ac6055b4
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-11-30 21:48:29 +0600

    register functionality added

--- src/components/Pages/Register/Register.js
+++ src/components/Pages/Register/Register.js
@@ -5,11 +5,49 @@ import { AuthContext } from '../../../Context/AuthProvider/AuthProvider';
  
 const Register = () => {
    const { register, handleSubmit } = useForm();
-
+   const { createUser, googleLogin, updateUser } = useContext(AuthContext)
+   const [userEmail, setUserEmail] = useState(null);
+   const navigate = useNavigate();
+   const onSubmit = data => {
+       createUser(data.email, data.password)
+           .then(user => {
+               const userInfo = {
+                   displayName: data.name
+               }
+               updateUser(userInfo)
+                   .then(() => {
+                       saveUserToDB(data.name, data.email)
+                   })
+                   .catch(err => console.error(err))
+           })
+           .catch(err => console.error(err))
+   };
+   const handleGoogleLogin = () => {
+       googleLogin()
+           .then(result => {
+               const user = result.user
+               saveUserToDB(user.displayName, user.email)
+           })
+           .catch(err => console.error(err))
+   }
+   const saveUserToDB = (name, email) => {
+       const user = { name, email };
+       fetch('http://localhost:4000/users', {
+           method: 'post',
+           headers: {
+               'content-type': 'application/json'
+           },
+           body: JSON.stringify(user)
+       })
+           .then(res => res.json())
+           .then(data => {
+               setUserEmail(user.email)
+           })
+   }
    return (
        <div className='h-[800px] flex justify-center items-center' >
            <div className='w-11/12 md:w-3/4 lg:w-4/12 shadow-xl p-10 rounded-xl'>
-               <form onSubmit={handleSubmit()}>
+               <form onSubmit={handleSubmit(onSubmit)}>
                    <h1 className='text-2xl text-center mb-5'>Register</h1>
                    <div className='form-contorl'>
                        <label className='label label-text'>Name</label>
@@ -29,7 +67,7 @@ const Register = () => {
                    <p className='text-center'>Already Have an Account? <Link className='text-rose-500 underline' to='/login'>Please Login</Link></p>
                    <div className="divider">OR</div>
                </form >
-               <button className='btn w-full bg-white text-accent text-lg font-normal hover:bg-black hover:text-white' >Continue With Google</button>
+               <button className='btn w-full bg-white text-accent text-lg font-normal hover:bg-black hover:text-white' onClick={handleGoogleLogin}>Continue With Google</button>
            </div >
        </div >
    );
