a165ea4a0511e894c9edbeea6a9f23a20a1b30e1
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-12-02 01:30:58 +0600

    footer added

--- src/components/Pages/Shared/Footer/Footer.js
+++ src/components/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className='mt-32 mb-10'>
+            <div className="footer">
+                <div className='md:mx-auto'>
+                    <span className="footer-title">Services</span>
+                    <Link to='/' className="link link-hover">Phone Setup</Link>
+                    <Link to='/' className="link link-hover">Battery Checkup</Link>
+                    <Link to='/' className="link link-hover">Phone Resell</Link>
+                </div>
+                <div className='md:mx-auto'>
+                    <span className="footer-title">Shop and Learn</span>
+                    <Link to='/' className="link link-hover">Iphone</Link>
+                    <Link to='/' className="link link-hover">Android Phone</Link>
+                    <Link to='/' className="link link-hover">Basic Phone</Link>
+                </div>
+                <div className='md:mx-auto'>
+                    <span className="footer-title">Our Address</span>
+                    <Link className="link link-hover">Gilatala - Jahanabad Cant</Link>
+                </div>
+            </div>
+            <div className='text-center mt-14'>
+                <p>Copyright © 2022 - All Rights Reserved.</p>
+            </div>
+        </footer>
     );
 };
 
