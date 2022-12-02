f1dab0fbd8cca07a500e67d3409a83d1918f773d
Tanvir Kabir <tanvir@Tanvirs-MacBook-Pro.local>
2022-12-02 11:54:27 +0600

    routes added on usertype

---
+++ src/components/Pages/Dashboard/AddProduct/AddProduct.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AddProduct = () => {
+    const handleAddProduct = event => {
+
+    }
+    return (
+        <div className='flex justify-center items-center' >
+            <div className='w-11/12 md:w-3/4 lg:w-8/12 shadow-xl p-10 rounded-xl'>
+                
+            </div >
+        </div >
+    );
+};
+
+export default AddProduct;
--- src/components/Routes/Router.js
+++ src/components/Routes/Router.js
@@ -2,7 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import DashboardLayout from "../Layouts/DashboardLayout";
 import Main from "../Layouts/Main";
 import Blogs from "../Pages/Blogs/Blogs";
+import AddProduct from "../Pages/Dashboard/AddProduct/AddProduct";
+import AllBuyers from "../Pages/Dashboard/AllBuyers/AllBuyers";
+import AllSellers from "../Pages/Dashboard/AllSellers/AllSellers";
 import Dashboard from "../Pages/Dashboard/Dashboard";
+import MyOrders from "../Pages/Dashboard/MyOrders/MyOrders";
+import MyProducts from "../Pages/Dashboard/MyProducts/MyProducts";
+import ReportedItems from "../Pages/Dashboard/ReportedItems/ReportedItems";
 import HomePage from "../Pages/Home/HomePage/HomePage";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
@@ -37,9 +43,33 @@ const router = createBrowserRouter([
         element: <PrivateRoute><DashboardLayout></DashboardLayout></PrivateRoute>,
         children: [
             {
-                path: '/dashboard',
+                path: '/dashboard/dashboard',
                 element: <Dashboard></Dashboard>
-            }
+            },
+            {
+                path: '/dashboard/addProduct',
+                element: <AddProduct></AddProduct>
+            },
+            {
+                path: '/dashboard/myProducts',
+                element: <MyProducts></MyProducts>
+            },
+            {
+                path: '/dashboard/myOrders',
+                element: <MyOrders></MyOrders>
+            },
+            {
+                path: '/dashboard/allSellers',
+                element: <AllSellers></AllSellers>
+            },
+            {
+                path: '/dashboard/allBuyers',
+                element: <AllBuyers></AllBuyers>
+            },
+            {
+                path: '/dashboard/reportedItems',
+                element: <ReportedItems></ReportedItems>
+            },
         ]
     }
 ])
